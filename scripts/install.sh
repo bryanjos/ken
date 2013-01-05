0f0e5a2805ee4489abfb1f873e68240299cc3823
Bryan Joseph <bryanjos@gmail.com>
2013-01-05 05:52:38 -0600

    Removed postgres and riak instructions from install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -6,6 +6,3 @@ sudo pip install pymongo
 sudo pip install redis
 sudo pip install -U pyyaml nltk
 sudo python -m nltk.downloader -d /usr/share/nltk_data all
-
-echo "Run create_db.sql, found in this folder, on a postgres db."
-echo "Information on installing Riak can be found at http://docs.basho.com/riak/latest/tutorials/installation/"
